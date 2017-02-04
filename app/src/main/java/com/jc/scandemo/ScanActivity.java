06adcdd3733a2470efc17f24042433ef5dbfb3f9
chenzujie <chenzujie@91yong.com>
2017-02-04 18:27:46 +0800

    init

---
+++ app/src/main/java/com/jc/scandemo/ScanActivity.java
@@ -0,0 +1,34 @@
+package com.jc.scandemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.client.android.CaptureActivity;
+import com.google.zxing.client.android.Intents;
+
+public class ScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+        Intent intent = new Intent(this, CaptureActivity.class);
+//        intent.setAction(Intents.Scan.MODE);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode==RESULT_OK){
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+            Bundle bundle = data.getExtras();
+            if (bundle != null) {
+            }
+        }
+
+    }
+}
