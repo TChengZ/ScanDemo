5e0bda7eb8629fc7d27de20702906d32025778a9
chenzujie <chenzujie@91yong.com>
2017-02-13 14:44:59 +0800

    修改解析数据quit也变成decode的错误

--- zxing/src/main/java/com/google/zxing/client/android/DecodeHandler.java
+++ zxing/src/main/java/com/google/zxing/client/android/DecodeHandler.java
@@ -59,7 +59,7 @@ final class DecodeHandler extends Handler {
     if(what == R.id.decode){
       decode((byte[]) message.obj, message.arg1, message.arg2);
     }
-    else if(what == R.id.decode){
+    else if(what == R.id.quit){
       running = false;
       Looper.myLooper().quit();
     }
